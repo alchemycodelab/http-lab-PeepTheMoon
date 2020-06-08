1e0b6471c59f086d704abcd448d3a0ca0a68821f
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-06-08 13:42:55 -0700

    adds get route with hi in the body

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,13 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('handles the / route', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
+
 
 });
